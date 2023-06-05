8afdf220ba899496c88d289c70457dae1dc9e7c0
manjeetverma <manjeetverma@gmail.com>
2023-06-05 13:58:12 +0530

    any request will call views/layouts/main.ejs, and it will render the respected page. included the stylesheet and script in the main.ejs header

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 const express=require('express')
 const app=express();
-const expresslayouts=require('express-ejs-layouts')
+const expresslayouts=require('express-ejs-layouts')//allow us to use layout template as ejs
 
 app.use(express.static('public'))//serving assets
 
 const PORT=5000 || process.env.PORT
 
 //templating engine
-app.use(expresslayouts)
-app.set('layout','./layouts/main')
-app.set('view engine','ejs')
+app.use(expresslayouts)//using expresslayouts as a middleware
+app.set('layout','./layouts/main')//setting main.ejs page as only rendering page.
+app.set('view engine','ejs')//setting view engine to ejs
 
 app.use('/',require('./server/routes/main'))
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,19 @@
 const express=require('express')
 const router=express.Router();
 
+//routes
+
 router.get('/',(req,res)=>{
-    res.send('home page')
+    const data={
+        title:'nodejs',
+        description:'blog website using node,express and mongodb'
+    }
+
+    res.render('home',{data})
+})
+
+router.get('/about',(req,res)=>{
+    res.render('about')
 })
 
 module.exports=router
